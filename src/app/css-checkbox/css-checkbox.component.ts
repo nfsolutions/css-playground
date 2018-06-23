db7913bb9cab3ad07fd6f814a8098727e8b0a02e
nydi <stefan.nydegger@gmail.com>
2018-06-24 00:00:40 +0200

    asdfsdf

---
+++ src/app/css-checkbox/css-checkbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css-checkbox',
+  templateUrl: './css-checkbox.component.html',
+  styleUrls: ['./css-checkbox.component.css']
+})
+export class CssCheckboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
